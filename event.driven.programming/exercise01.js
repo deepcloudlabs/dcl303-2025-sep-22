b1b9ca0764aeac6189a77575eb2532307fe173bd
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-24 15:21:39 +0300

    initial commit

---
+++ event.driven.programming/exercise01.js
@@ -0,0 +1,11 @@
+const timer_id = setInterval(() => {
+    console.log("Time is ticking...");
+}, 1_000);
+
+setTimeout(()=>{
+    clearInterval(timer_id);
+}, 10_000)
+
+fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+   .then(res => res.json())
+   .then( ({price}) => console.log(price));
