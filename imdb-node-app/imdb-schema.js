9d3405966615de443b98d40659ff92a6460a09c1
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-25 14:54:45 +0300

    initial commit

---
+++ imdb-node-app/imdb-schema.js
@@ -0,0 +1,15 @@
+/*
+  {
+    _id: 30,
+    directors: [ { _id: 31, imdb: 'nm1470993', name: 'Scott Mann' } ],
+    genres: [ { name: 'Action', _id: 5 } ],
+    imdb: 'tt0471041',
+    title: 'The Tournament',
+    year: 2009
+  }
+ */
+
+const {Schema} = require("mongoose");
+const MovieSchema = new Schema({
+
+});
